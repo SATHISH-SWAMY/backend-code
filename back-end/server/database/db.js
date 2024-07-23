ba13b015889f5a448e43e8d7ab3c5d22013e10e7
Sathish Swamy <145578184+SATHISH-SWAMY@users.noreply.github.com>
2024-07-23 17:32:56 +0530

    Update db.js

--- back-end/server/database/db.js
+++ back-end/server/database/db.js
@@ -1,22 +1,18 @@
 import mongoose from 'mongoose';
-
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-
-const connection = () => {
-    const DB_URI =`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.43fzhth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-    try{
-          mongoose.connect(DB_URI, {useNewUrlParser:true});
-          console.log('database connected successfully')
-    } catch (error){
-        console.log('error while connection with the database' , error.message);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.error('Could not connect to database:', error.message);
     }
-}
+};
 
-
-export default connection;
- 
- 
-
+export default connectDB;
